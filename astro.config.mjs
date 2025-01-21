25abf38b4e140e4206ccf02fd4d033d36c9d83f1
kekeandzeyu <kekeandzeyu@gmail.com>
2025-01-21 13:20:53 +0800

    add collapsible code blocks in notes content

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import remarkMath from 'remark-math';
 import rehypeKatex from 'rehype-katex';
 import mdx from '@astrojs/mdx';
 
+import expressiveCode from 'astro-expressive-code';
+
 export default defineConfig({
   markdown: {
     remarkPlugins: [remarkMath],
@@ -11,5 +13,5 @@ export default defineConfig({
       theme: 'github-light',
     }
   },
-  integrations: [mdx()],
+  integrations: [expressiveCode(), mdx()],
 });
