e85fda36f36ef4ea9cc45714924778926003a3bd
kekeandzeyu <kekeandzeyu@gmail.com>
2025-01-23 13:51:06 +0800

    add more notes and fix tab linking error

--- ec.config.mjs
+++ ec.config.mjs
@@ -1,8 +1,9 @@
 import { defineEcConfig } from 'astro-expressive-code'
 import { pluginCollapsibleSections } from '@expressive-code/plugin-collapsible-sections'
+import { pluginLineNumbers } from '@expressive-code/plugin-line-numbers'
 
 export default defineEcConfig({
-  plugins: [pluginCollapsibleSections()],
+  plugins: [pluginCollapsibleSections(), pluginLineNumbers()],
   defaultProps: {
     collapseStyle: 'collapsible-start',
   },
